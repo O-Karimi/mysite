b7195900d191a5dd5352282cbf4e1aca09415194
Omid Karimi <omidkarimi54@gmail.com>
2024-04-09 17:06:44 +0330

    template and inheritence completed!

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from blog.views import blog
+from blog.views import blog , blog_single
 
 app_name = 'blog'
 
 urlpatterns = [
     path('',blog,name='blog'),
+    path('single/',blog_single,name='single-blog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def blog(requeset):
-    return render(requeset,'blog.html')
+    return render(requeset,'blog/blog.html')
+
+def blog_single(requeset):
+    return render(requeset,'blog/single-blog.html')
